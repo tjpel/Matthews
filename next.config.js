fc26d72347991afddf1ce8493a2b7540b6ae71b2
Coderman562 <travispeng@gmail.com>
2023-09-13 01:08:20 -0700

    add API frontend route handler

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  features: {
+    serverActions: true,
+  },
+  experimental: {
+    appDir: true,
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:8000/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
   images: {
     domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh']
   }
