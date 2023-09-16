82f17ac4cc9fe0ccdb8962b3001685afabc8f014
Coderman562 <travispeng@gmail.com>
2023-09-16 16:16:11 -0700

    add properties model and define route requirements

--- backend/app/api/v1/property.py
+++ backend/app/api/v1/property.py
@@ -11,7 +11,8 @@ from app.auth import UserAuth
 from app.auth.google import client, get_user_info
 from app.core.config import config
 from app.db.session import AsyncSessionDep
-from app.services import users, sessions
+from app.services import properties
+
 
 REDIRECT = f"{config.server_url}{config.api_v1_route}/property"
 SCOPES = [
@@ -22,15 +23,17 @@ SCOPES = [
 routes = APIRouter(prefix="/property")
 
 @routes.get("/predict")
-async def predict_property_value():
+async def predict_property_value(property: properties.Property, user_inputs: properties.User_inputs) -> str:
+    # Gather data from property using APIs
+    # Store data in database
+
+    # Call ML model
+
+    # Return predicted value
+
     return {"message": "Hello World"}
 
 # Password Credentials
 @routes.post("/contact")
-async def add_contact_info(session: AsyncSessionDep, login: users.Login) -> str:
-    user = await users.verify_login(session, login)
-    # Will send out email with contact info to user
-    if user:
-        return sessions.create_token(user)
-    else:
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+async def add_contact_info(property: properties.Property) -> str:
+    return {"property": property}
