8402da0dcf4d6b4daea8ab33d53359a1b36c9ad6
Coderman562 <travispeng@gmail.com>
2023-09-16 15:41:16 -0700

    add property file into api route, add testing route to frontend

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 
 from app.auth import UserAuth
+
 from app.core.config import config
 from . import auth
+from . import property
 
 routes = APIRouter(prefix=config.api_v1_route)
 
 routes.include_router(auth.routes)
-
+routes.include_router(property.routes)
 
 @routes.get("/test_route")
 async def test_route(user: UserAuth):
