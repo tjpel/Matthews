bc7d7d59eeac641caf4d46b34dd544bfd88e5200
Coderman562 <126994465+Coderman562@users.noreply.github.com>
2023-09-16 15:13:28 -0700

    update navbar hrefs

--- frontend/src/app/dashboard/navbar.tsx
+++ frontend/src/app/dashboard/navbar.tsx
@@ -8,8 +8,8 @@ import { signIn, signOut } from 'next-auth/react';
 import Image from 'next/image';
 
 const navigation = [
-  { name: 'Dashboard', href: '/' },
-  { name: 'Playground', href: '/playground' }
+  { name: 'Dashboard', href: '/dashboard' },
+  { name: 'Playground', href: '/dashboard/playground' }
 ];
 
 function classNames(...classes: string[]) {
