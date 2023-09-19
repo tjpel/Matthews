06315d8688f2439899856d4a380b627863a36934
Coderman562 <travispeng@gmail.com>
2023-09-18 20:10:10 -0700

    update tailwind configuration for shadcn

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,10 +1,11 @@
-import { clsx, type ClassValue } from 'clsx'
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 import { customAlphabet } from 'nanoid'
-import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
 
 export const nanoid = customAlphabet(
   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
