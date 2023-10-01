62555cf263ea519c48899b6bff2d9b3e2f2eb804
Coderman562 <travispeng@gmail.com>
2023-09-30 19:23:08 -0700

    update next.config.js backend calling

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
         source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/api/:path*'
-            : '/api/',
+            ? 'http://127.0.0.1:8000/api/:path*'  // For local development
+            : 'https://matthews-backend.fly.dev/api/:path*',  // For production
       },
     ];
   },
