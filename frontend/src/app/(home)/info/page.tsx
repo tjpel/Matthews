0c1d913c1a681753830fd0d1240cc47b71baea1f
Coderman562 <126994465+Coderman562@users.noreply.github.com>
2023-10-04 21:14:30 -0700

    update /info for proper eslint format

--- frontend/src/app/(home)/info/page.tsx
+++ frontend/src/app/(home)/info/page.tsx
@@ -1,32 +1,32 @@
-export default () => {
+const faqsList = [
+    {
+        q: "What are some random questions to ask?",
+        a: "That's exactly the reason we created this random question generator. There are hundreds of random questions to choose from so you're able to find the perfect random question."
+    },
+    {
+        q: "Do you include common questions?",
+        a: "This generator doesn't include most common questions. The thought is that you can come up with common questions on your own so most of the questions in this generator."
+    },
+    {
+        q: "Can I use this for 21 questions?",
+        a: "Yes! there are two ways that you can use this question generator depending on what you're after. You can indicate that you want 21 questions generated."
+    },
+    {
+        q: "Are these questions for girls or for boys?",
+        a: "The questions in this generator are gender neutral and can be used to ask either male of females (or any other gender the person identifies with)."
+    },
+    {
+        q: "What are some random questions to ask?",
+        a: "That's exactly the reason we created this random question generator. There are hundreds of random questions to choose from so you're able to find the perfect random question to ask friends."
+    },
+    {
+        q: "What do you wish you had more talent doing?",
+        a: "If you've been searching for a way to get random questions, you've landed on the correct webpage. We created the Random Question Generator to ask you as many random questions as your heart desires."
+    },
+]
 
-    const faqsList = [
-        {
-            q: "What are some random questions to ask?",
-            a: "That's exactly the reason we created this random question generator. There are hundreds of random questions to choose from so you're able to find the perfect random question."
-        },
-        {
-            q: "Do you include common questions?",
-            a: "This generator doesn't include most common questions. The thought is that you can come up with common questions on your own so most of the questions in this generator."
-        },
-        {
-            q: "Can I use this for 21 questions?",
-            a: "Yes! there are two ways that you can use this question generator depending on what you're after. You can indicate that you want 21 questions generated."
-        },
-        {
-            q: "Are these questions for girls or for boys?",
-            a: "The questions in this generator are gender neutral and can be used to ask either male of females (or any other gender the person identifies with)."
-        },
-        {
-            q: "What are some random questions to ask?",
-            a: "That's exactly the reason we created this random question generator. There are hundreds of random questions to choose from so you're able to find the perfect random question to ask friends."
-        },
-        {
-            q: "What do you wish you had more talent doing?",
-            a: "If you've been searching for a way to get random questions, you've landed on the correct webpage. We created the Random Question Generator to ask you as many random questions as your heart desires."
-        },        
-    ]
-  
+export default function InfoPage() {
+
     return (
         <section className="leading-relaxed max-w-screen-xl mt-12 mx-auto px-4 md:px-8">
             <div className="space-y-3 text-center">
@@ -40,7 +40,7 @@ export default () => {
             <div className="mt-14 gap-4 sm:grid sm:grid-cols-2 lg:grid-cols-3">
                 {
                     faqsList.map((item, idx) => (
-                        <div 
+                        <div
                             className="space-y-3 mt-5"
                             key={idx}
                         >
