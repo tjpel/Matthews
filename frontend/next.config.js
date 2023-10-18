45ca915c9c3dd6059b5b1abb497c5625a7495e95
Coderman562 <126994465+Coderman562@users.noreply.github.com>
2023-10-18 13:03:18 -0700

    update backendURL rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${backendUrl}/api/:path*`,
+        destination: `${backendUrl}/:path*`,
       },
     ];
   },
