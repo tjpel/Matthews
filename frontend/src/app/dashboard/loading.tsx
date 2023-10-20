b06472ebb9db1963cab7cb081a5d93c916cdebe0
Coderman562 <126994465+Coderman562@users.noreply.github.com>
2023-10-20 11:09:05 -0700

    add use client to fix deployment

--- frontend/src/app/dashboard/loading.tsx
+++ frontend/src/app/dashboard/loading.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import { Title, Text } from '@tremor/react';
 import Search from './search';
 
-export default async function Loading() {
+export default function Loading() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Title>Users</Title>
