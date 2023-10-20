aa4347354bbbe5fe39e1ce7113ce06468ed27e3d
Coderman562 <126994465+Coderman562@users.noreply.github.com>
2023-10-19 18:36:16 -0700

    __init__ now imports files within folders

--- backend/app/ai/__init__.py
+++ backend/app/ai/__init__.py
@@ -8,14 +8,15 @@ current_dir = Path(__file__).parent.resolve()
 
 def load_all_models(directory: Path) -> Dict[str, BaseEstimator]:
     models = {}
-    for filename in os.listdir(directory):
-        if filename.endswith(".pkl"):
-            model_name = filename.split(".")[0]
-            path = directory.joinpath(filename)
-            with open(path, "rb") as file:
-                model = pickle.load(file)
-            models[model_name] = model
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith(".pkl"):
+                model_name = filename.split(".")[0]
+                path = Path(dirpath).joinpath(filename)
+                with open(path, "rb") as file:
+                    model = pickle.load(file)
+                models[model_name] = model
     return models
 
-# Load all models in the current directory
+# Load all models in the current directory and its subdirectories
 all_models = load_all_models(current_dir)
