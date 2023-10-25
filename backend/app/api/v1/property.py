acb2333d3525c4fa51736fe33c420f9cedcb677c
Diego Sesmas <diegos7167@gmail.com>
2023-10-24 20:59:31 -0700

    Cleaned up backend routes

--- backend/app/api/v1/property.py
+++ backend/app/api/v1/property.py
@@ -1,70 +1,18 @@
 from redfin import Redfin
 
 from fastapi import APIRouter, Body, HTTPException
-from typing import Any
+from typing import Annotated
 
 import pandas as pd
 import random
 
-from app.services import properties
-import app.ai as ai
+from app.services.properties import UserInputs, ContactInfo
 
 routes = APIRouter(prefix="/property")
 
 
-@routes.get("/get-property-data")
-def get_property_data(address: str):
-    # Initialize Redfin API client
-    client = Redfin()
-
-    print(address)
-
-    # Search for the property by address
-    response = client.search(address)
-
-    # Check if 'exactMatch' exists in the payload
-    if "payload" in response and "exactMatch" in response["payload"]:
-        url = response["payload"]["exactMatch"]["url"]
-    else:
-        raise HTTPException(status_code=404, detail="Exact match not found")
-
-    # Fetch initial property info
-    initial_info = client.initial_info(url)
-
-    # Check if 'propertyId' exists in the payload
-    property_id = initial_info["payload"].get("propertyId")
-    if not property_id:
-        raise HTTPException(status_code=404, detail="Property ID not found")
-
-    # Fetch MLS data
-    mls_data = client.below_the_fold(property_id)
-    mls_payload = mls_data["payload"]
-
-    # Check if 'listingId' exists in the payload
-    listing_id = initial_info["payload"].get("listingId")
-    if listing_id:
-        avm_details = client.avm_details(property_id, listing_id)
-        avm_payload = avm_details.get("payload", {})
-    else:
-        print("listingId not found")
-        avm_payload = {}
-
-    return {
-        "response": response,
-        "initial_info": initial_info,
-        "mls_data": mls_payload,
-        "avm_details": avm_payload,
-    }
-
-
 @routes.post("/predict")
-def predict_property_value(
-    address: properties.Address = Body(...),
-    user_inputs: properties.UserInputs = Body(...)
-):
-    # Get user inputs from request body
-    print(user_inputs)
-
+def predict_property_value(user_inputs: UserInputs):
     # Convert user_inputs to a Pandas DataFrame
     user_inputs_dict = user_inputs.dict()
     user_inputs_df = pd.DataFrame([user_inputs_dict])
@@ -87,12 +35,14 @@ def predict_property_value(
     increment = 100000  # 100,000
 
     # Generate a random number between min_value and max_value in increments of increment
-    random_prediction = random.randint(min_value // increment, max_value // increment) * increment
+    random_prediction = (
+        random.randint(min_value // increment, max_value // increment) * increment
+    )
 
-    return { "prediction": random_prediction }
+    return {"prediction": random_prediction}
 
 
 # Route to store contact info in google analytics
 @routes.post("/contact")
-async def add_contact_info(contact: properties.ContactInfo):
-    return {"property": property}
+async def add_contact_info(contact: ContactInfo):
+    ...
