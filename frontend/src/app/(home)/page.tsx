119c0fc4e8f498f3dbc97f5e1e276a0ca0554d12
reerosebee <riyabhattac@gmail.com>
2023-10-27 13:57:39 -0700

    modified:   frontend/src/app/(home)/page.tsx;  links website

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
               }}
             >
               <Link
-                href="/about"
+                href="https://tayloravakian.carrd.co/"
                 className="group rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#1E2B3A] text-[#f5f7f9] no-underline active:scale-95 scale-100 duration-75"
                 style={{
                   boxShadow: '0 1px 1px #0c192714, 0 1px 3px #0c192724'
