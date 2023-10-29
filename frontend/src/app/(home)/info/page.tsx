90b965173cadcb6ad5a5cade5a4665828fcbe749
jzhong0821 <jzhong0821@gmail.com>
2023-10-29 15:02:46 -0700

    success page and info page

--- frontend/src/app/(home)/info/page.tsx
+++ frontend/src/app/(home)/info/page.tsx
@@ -1,58 +1,51 @@
-export default () => {
+'use client';
 
-  const team = [
-      {
-          avatar: "https://www.matthews.com/wp-content/uploads/2018/10/Taylor-Avakian-Use-1.jpg",
-          name: "Taylor Avakain",
-          title: "Associate Vice President of Multifamily",
-          desc: "Taylor Avakian is a premier client advisor for the acquisition and disposition of multifamily assets nationwide. His primary focus is on advancing his client’s positions by observing market trends and capitalizing on opportunities. Taylor has worked on over 100 transactions, both as an advisor and a principal and sold over $175M in real estate. He is adept at managing the process from beginning to end, including due diligence, financing, marketing, and sales. Taylor’s extensive knowledge of the market and his ability to identify opportunities are what make him stand out from other agents. He has been recognized as one of his region’s top commercial real estate agents, serving clients from all over the United States. His clients include some of the most notable companies in the world, and he prides himself on providing them with exceptional service.",
-          linkedin: "javascript:void(0)",
-          twitter: "javascript:void(0)",
-      },
-  ]
+import Link from 'next/link';
 
+export default function AboutUsPage() {
   return (
-      <section className="py-14">
-          <div className="max-w-screen-xl mx-auto px-4 text-center md:px-8">
-              <div className="max-w-xl mx-auto">
-                  <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-                      Meet our team
-                  </h3>
-                  <p className="text-gray-600 mt-3">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy.
-                  </p>
-              </div>
-              <div className="mt-12">
-                  <ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
-                      {
-                          team.map((item, idx) => (
-                              <li key={idx}>
-                                  <div className="w-24 h-24 mx-auto">
-                                      <img
-                                          src={item.avatar}
-                                          className="w-full h-full rounded-full"
-                                          alt=""
-                                      />
-                                  </div>
-                                  <div className="mt-2">
-                                      <h4 className="text-gray-700 font-semibold sm:text-lg">{item.name}</h4>
-                                      <p className="text-indigo-600">{item.title}</p>
-                                      <p className="text-gray-600 mt-2">{item.desc}</p>
-                                      <div className="mt-4 flex justify-center gap-4 text-gray-400">
-                                          <a href={item.twitter}>
-                                              <svg className="w-5 h-5 duration-150 hover:text-gray-500" fill="currentColor" viewBox="0 0 48 48"><g clip-path="url(#clip0_17_80)"><path fill="currentColor" d="M15.1 43.5c18.11 0 28.017-15.006 28.017-28.016 0-.422-.01-.853-.029-1.275A19.998 19.998 0 0048 9.11c-1.795.798-3.7 1.32-5.652 1.546a9.9 9.9 0 004.33-5.445 19.794 19.794 0 01-6.251 2.39 9.86 9.86 0 00-16.788 8.979A27.97 27.97 0 013.346 6.299 9.859 9.859 0 006.393 19.44a9.86 9.86 0 01-4.462-1.228v.122a9.844 9.844 0 007.901 9.656 9.788 9.788 0 01-4.442.169 9.867 9.867 0 009.195 6.843A19.75 19.75 0 010 39.078 27.937 27.937 0 0015.1 43.5z" /></g><defs><clipPath id="clip0_17_80"><path fill="currentColor" d="M0 0h48v48H0z" /></clipPath></defs></svg>
-                                          </a>
-                                          <a href={item.linkedin}>
-                                              <svg className="w-5 h-5 duration-150 hover:text-gray-500" fill="none" viewBox="0 0 48 48"><g clip-path="url(#clip0_17_68)"><path fill="currentColor" d="M44.447 0H3.544C1.584 0 0 1.547 0 3.46V44.53C0 46.444 1.584 48 3.544 48h40.903C46.407 48 48 46.444 48 44.54V3.46C48 1.546 46.406 0 44.447 0zM14.24 40.903H7.116V17.991h7.125v22.912zM10.678 14.87a4.127 4.127 0 01-4.134-4.125 4.127 4.127 0 014.134-4.125 4.125 4.125 0 010 8.25zm30.225 26.034h-7.115V29.766c0-2.653-.047-6.075-3.704-6.075-3.703 0-4.265 2.896-4.265 5.887v11.325h-7.107V17.991h6.826v3.13h.093c.947-1.8 3.272-3.702 6.731-3.702 7.21 0 8.541 4.744 8.541 10.912v12.572z" /></g><defs><clipPath id="clip0_17_68"><path fill="currentColor" d="M0 0h48v48H0z" /></clipPath></defs></svg>
-                                          </a>
-                                      </div>
-                                  </div>
-                              </li>
-                          ))
-                      }
-                  </ul>
-              </div>
+    <main className="py-14">
+      <div className="max-w-screen-xl mx-auto px-4 text-[#1a2b3b] md:px-8">
+        <div className="max-w-lg mx-auto space-y-3 sm:text-center">
+          <p className="text-[#1E2B3A] text-3xl font-semibold sm:text-4xl">
+            Taylor Avakian
+          </p>
+          <img 
+            src="https://ca-times.brightspotcdn.com/dims4/default/2d65279/2147483647/strip/true/crop/840x840+0+0/resize/840x840!/format/webp/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fe6%2Fb6%2F67a77bb642e0b232b4c7bded2eea%2Ftaylor-avakian-crop.jpg"
+            alt="Taylor Avakian"
+            className="mx-auto rounded-full border-4 border-[#1E2B3A] w-48 h-48 object-cover"
+          />
+          <div className="mt-12">
+              <p className=" mb-4">
+                  Taylor Avakian is a premier client advisor for the acquisition and disposition of multifamily assets nationwide. His primary focus is on advancing his client’s positions by observing market trends and capitalizing on opportunities. Taylor has worked on over 100 transactions, both as an advisor and a principal and sold over $175M in real estate.
+              </p>
+              <p >
+                  He is adept at managing the process from beginning to end, including due diligence, financing, marketing, and sales. Taylor’s extensive knowledge of the market and his ability to identify opportunities are what make him stand out from other agents. He has been recognized as one of his region’s top commercial real estate agents, serving clients from all over the United States. His clients include some of the most notable companies in the world, and he prides himself on providing them with exceptional service.
+              </p>
           </div>
-      </section>
-  )
+        </div>
+        <div className="mt-8 max-w-lg mx-auto flex justify-between gap-x-4">
+          <Link
+            href="/"
+            className="group w-1/2 px-4 py-2 font-semibold transition-all flex items-center justify-center bg-[#1E2B3A] text-white hover:bg-[#1E2B3A] rounded-lg no-underline active:scale-95 scale-100 duration-75"
+            style={{
+              boxShadow: '0 1px 1px #0c192714, 0 1px 3px #0c192724'
+            }}
+          >
+            Back to home
+          </Link>
+
+          <Link
+            href="https://www.matthews.com/agents/taylor-avakian/"
+            className="group w-1/2 px-4 py-2 font-semibold transition-all flex items-center justify-center bg-[#1E2B3A] text-white hover:bg-[#1E2B3A] rounded-lg no-underline active:scale-95 scale-100 duration-75"
+            style={{
+              boxShadow: '0 1px 1px #0c192714, 0 1px 3px #0c192724'
+            }}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
