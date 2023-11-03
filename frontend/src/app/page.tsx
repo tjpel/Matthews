6e4aae620bbb9b5e622795a1ee5d7db3a6a76a06
Coderman562 <126994465+Coderman562@users.noreply.github.com>
2023-11-02 18:26:23 -0700

    Refactor page component styles and structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,44 +1,236 @@
-import { Arrow } from '@/svg/arrow';
-import { GradientCanvas } from './gradient';
+import { Arrow } from "@/svg/arrow";
+import { GradientCanvas } from "./gradient";
 
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
 export default function Page() {
-  return <>
-    <div className={styles.front}>
-      <div className={styles.title}>
-        <h2 className={styles.taylor}>Taylor Avakian</h2>
+  return (
+    <>
+      <div className={styles.front}>
+        <div className={styles.title}>
+          <h2 className={styles.taylor}>Taylor Avakian</h2>
 
-        <h1>
-          Find Your <br />
-          Multifamily <span className={styles.primary}>Valuation.</span>
-        </h1>
+          <h1>
+            Find Your <br />
+            Multifamily <span className={styles.primary}>Valuation.</span>
+          </h1>
 
-        <div className={styles.recs}>
-          <div>
-            <h3>Valuator</h3>
-
-            Use our AI for for free to get an estimate on your multifamily property's value.
+          <div className={styles.recs}>
+            <div>
+              <h3>Valuator</h3>
+              Use our AI for for free to get an estimate on your multifamily
+              property&apos;s value.
+            </div>
+            <div>
+              <h3>Connect</h3>
+              Connect with us to gain invaluable insights from industry leaders
+              and experts.
+            </div>
           </div>
-          <div>
-            <h3>Connect</h3>
 
-            Connect with us to gain invaluable insights from industry leaders and experts.
+          <div className={styles.actions}>
+            <a className="button">All About Us</a>
+
+            <a className="button">Contact Us</a>
+
+            <a className="button primary" href="./estimator">
+              Try it out <Arrow />
+            </a>
           </div>
         </div>
 
-        <div className={styles.actions}>
-          <a className='button'>All About Us</a>
-
-          <a className='button'>Contact Us</a>
-
-          <a className='button primary' href='./estimator'>Try it out <Arrow /></a>
-        </div>
+        <GradientCanvas />
       </div>
 
-      <GradientCanvas />
-    </div>
+      <div className={styles.bar} />
+    </>
+  );
+}
+{
+  /* <AnimatePresence>
+<div className="min-h-[100vh] sm:min-h-screen w-screen flex flex-col relative bg-[#F2F3F5] font-inter overflow-hidden">
+  <svg
+    style={{ filter: 'contrast(125%) brightness(110%)' }}
+    className="fixed z-[1] w-full h-full opacity-[35%]"
+  >
+    <filter id="noise">
+      <feTurbulence
+        type="fractalNoise"
+        baseFrequency=".7"
+        numOctaves="3"
+        stitchTiles="stitch"
+      ></feTurbulence>
+      <feColorMatrix type="saturate" values="0"></feColorMatrix>
+    </filter>
+    <rect width="100%" height="100%" filter="url(#noise)"></rect>
+  </svg>
+  <main className="flex flex-col justify-center h-[90%] static md:fixed w-screen overflow-hidden grid-rows-[1fr_repeat(3,auto)_1fr] z-[100] pt-[30px] pb-[320px] px-4 md:px-20 md:py-0">
+    <motion.h1
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.15,
+        duration: 0.95,
+        ease: [0.165, 0.84, 0.44, 1]
+      }}
+      className="whitespace-nowrap font-extrabold text-[4vw] md:text-[25px] font-inter text-[#1E2B3A] z-[100]"
+    >
+      TAYLOR AVAKIAN
+    </motion.h1>
 
-    <div className={styles.bar} />
-  </>;
+    <motion.h1
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.15,
+        duration: 0.95,
+        ease: [0.165, 0.84, 0.44, 1]
+      }}
+      className="relative md:ml-[-10px] md:mb-[37px] font-extrabold text-[16vw] md:text-[130px] font-inter text-[#1E2B3A] leading-[0.9] tracking-[-2px] z-[100]"
+    >
+      Find Your <br />
+      Multifamily <span className="text-[#407BBF]">Valuation</span>
+      <span className="font-inter text-[#407BBF]">.</span>
+    </motion.h1>
+    <motion.div
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.15,
+        duration: 0.95,
+        ease: [0.165, 0.84, 0.44, 1]
+      }}
+      className="flex flex-row justify-center z-20 mx-0 mb-0 mt-8 md:mt-0 md:mb-[35px] max-w-2xl md:space-x-8"
+    >
+      <div className="w-1/2">
+        <h2 className="flex items-center font-semibold text-[1em] text-[#1a2b3b]">
+          Valuator
+        </h2>
+        <p className="text-[14px] leading-[20px] text-[#1a2b3b] font-normal">
+          Use our AI for for free to get an estimate on your multifamily
+          property&apos;s value.
+        </p>
+      </div>
+      <div className="w-1/2">
+        <h2 className="flex items-center font-semibold text-[1em] text-[#1a2b3b]">
+          Connect
+        </h2>
+        <p className="text-[14px] leading-[20px] text-[#1a2b3b] font-normal">
+          Connect with us to gain invaluable insights from industry
+          leaders and experts.
+        </p>
+      </div>
+    </motion.div>
+
+    <div className="flex gap-[15px] mt-8 md:mt-0">
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.45,
+          duration: 0.55,
+          ease: [0.075, 0.82, 0.965, 1]
+        }}
+      >
+        <Link
+          href="https://tayloravakian.carrd.co/"
+          className="group rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#1E2B3A] text-[#f5f7f9] no-underline active:scale-95 scale-100 duration-75"
+          style={{
+            boxShadow: '0 1px 1px #0c192714, 0 1px 3px #0c192724'
+          }}
+        >
+          All About Us
+        </Link>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.55,
+          duration: 0.55,
+          ease: [0.075, 0.82, 0.965, 1]
+        }}
+      >
+        <Link
+          href="/contact"
+          target="_blank"
+          className="group rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#4f6170] text-[#f5f7f9] no-underline active:scale-95 scale-100 duration-75"
+        >
+          Contact Us
+        </Link>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.65,
+          duration: 0.55,
+          ease: [0.075, 0.82, 0.965, 1]
+        }}
+      >
+        <Link
+          href="/estimator"
+          className="group rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
+          style={{
+            boxShadow: '0 1px 1px #0c192714, 0 1px 3px #0c192724'
+          }}
+        >
+          <span className="mr-2"> Try it out </span>
+          <svg
+            className="w-5 h-5"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.75 6.75L19.25 12L13.75 17.25"
+              stroke="#1E2B3A"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M19 12H4.75"
+              stroke="#1E2B3A"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </Link>
+      </motion.div>
+    </div>
+  </main>
+
+  <div
+    className="fixed top-0 right-0 w-[80%] md:w-1/2 h-screen bg-[#1F2B3A]/20"
+    style={{
+      clipPath:
+        'polygon(100px 0,100% 0,calc(100% + 225px) 100%, 480px 100%)'
+    }}
+  ></div>
+
+  <motion.canvas
+    initial={{
+      filter: 'blur(20px)'
+    }}
+    animate={{
+      filter: 'blur(0px)'
+    }}
+    transition={{
+      duration: 1,
+      ease: [0.075, 0.82, 0.965, 1]
+    }}
+    style={{
+      clipPath:
+        'polygon(100px 0,100% 0,calc(100% + 225px) 100%, 480px 100%)'
+    }}
+    id="gradient-canvas"
+    data-transition-in
+    className="z-50 fixed top-0 right-[-2px] w-[80%] md:w-1/2 h-screen bg-[#c3e4ff]"
+  ></motion.canvas>
+  <div className="h-[60px] bg-[#1D2B3A] fixed bottom-0 z-20 w-full flex flex-row items-center justify-evenly"></div>
+</div>
+</AnimatePresence> */
 }
